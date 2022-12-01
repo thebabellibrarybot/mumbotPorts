25fddaea027a217d654e91c825bdb565e3a568dd
thebabellibrarybot <jack@thebabel.org>
2022-11-30 20:30:06 -0500

    .

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -34,7 +34,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div class = {visible ? 'listobj' : 'invisible'}>
+                <div className = {visible ? 'listobj' : 'invisible'}>
                     <ListObj/>
                 </div>
             </div>
